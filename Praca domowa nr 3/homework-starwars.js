d53df72268e344f94d994f2bab9ef76325dd77f8
djakimiuk <djakimiuk@outlook.com>
2022-11-16 23:16:53 +0100

    ex. 2.1 solved

--- Praca domowa nr 3/homework-starwars.js
+++ Praca domowa nr 3/homework-starwars.js
@@ -1,24 +1,42 @@
-const films = require('./sw-films.json');
-const planets = require('./sw-planets.json');
-const people = require('./sw-people.json');
-const starships = require('./sw-starships.json');
+const films = require("./sw-films.json");
+const planets = require("./sw-planets.json");
+const people = require("./sw-people.json");
+const starships = require("./sw-starships.json");
 
 // count sum of all starships cost from episodes 4-6
 console.log(
-  'Sum of all starships cost from episodes 4 - 6 is: ' +
+  "Sum of all starships cost from episodes 4 - 6 is: " +
     sumAllStarshipsCostFromEpisodes(4, 6)
 );
 
 function sumAllStarshipsCostFromEpisodes(startEp, endEp) {
   let sum = 0;
-  // TODO
+  let starshipURLarray = [];
+  films
+    .filter(
+      (currentElement) =>
+        currentElement.episode_id >= startEp &&
+        currentElement.episode_id <= endEp
+    )
+    .forEach((currentElement) =>
+      starshipURLarray.push(...currentElement.starships)
+    );
+  sum = starships
+    .filter(
+      (currentElement) =>
+        starshipURLarray.includes(currentElement.url) &&
+        currentElement.cost_in_credits !== "unknown"
+    )
+    .reduce((accumulator, currentElement) => {
+      return accumulator + parseFloat(currentElement.cost_in_credits);
+    }, 0);
   return sum;
 }
 
 // find the fastest starship you can afford having 8500000 credits
 
 console.log(
-  'Fastest ship I can get for up to 8500000 is: ' +
+  "Fastest ship I can get for up to 8500000 is: " +
     getFastestShipFor(8500000).name
 );
 
@@ -31,8 +49,8 @@ function getFastestShipFor(money) {
 // find planet name with the lowest difference between the rotation period and orbital period
 
 console.log(
-  'Planet name with the lowest difference between the rotation period and orbital period is: ' +
-    getPlanetNameWithLowestDifference('rotation_period', 'orbital_period')
+  "Planet name with the lowest difference between the rotation period and orbital period is: " +
+    getPlanetNameWithLowestDifference("rotation_period", "orbital_period")
 );
 
 function getPlanetNameWithLowestDifference(key1, key2) {
@@ -44,7 +62,7 @@ function getPlanetNameWithLowestDifference(key1, key2) {
 // map all starships with crew <= 4 that were created between 10 dec 2014 and 15 dec 2014
 
 console.log(
-  'Ships with max crew of 4 created between 10.12.2014 - 12.12.2014 number is: ' +
+  "Ships with max crew of 4 created between 10.12.2014 - 12.12.2014 number is: " +
     getCrewShipFrom(4, new Date(2014, 12, 10), new Date(2014, 12, 12)).length
 );
 
@@ -57,7 +75,7 @@ function getCrewShipFrom(maxCrew, dateStart, dateEnd) {
 // create an array of people’s names from episodes 1 and 5 sorted by the diameter of origin planet low to high
 
 console.log(
-  'People from ep 1 - 5 sorted by origin planet diameter low to high: ' +
+  "People from ep 1 - 5 sorted by origin planet diameter low to high: " +
     getPeopleSortedByOriginPlanetDiameter(1, 5)
 );
 
