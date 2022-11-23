738bfa4524745735a48299fec8c464828945a50c
djakimiuk <djakimiuk@outlook.com>
2022-11-23 22:44:12 +0100

    ex. 2 solved

--- Praca domowa nr 4/homework-2.js
+++ Praca domowa nr 4/homework-2.js
@@ -4,22 +4,25 @@
 // RUN npm install IN THIS FOLDER BEFORE RUNNING YOUR CODE!
 const fetch = require("node-fetch");
 
-function getCharactersFromEpisode(episodeNumber) {
-  const episodesURL = fetch(
+async function getCharactersFromEpisode(episodeNumber) {
+  const characterURL = "https://rickandmortyapi.com/api/character/";
+  const charactersIDs = await fetch(
     `https://rickandmortyapi.com/api/episode/${episodeNumber}`
-  );
-  const characters = fetch(`https://rickandmortyapi.com/api/character/`);
-  const somearray = [];
-  const resultCharacters = somearray.push(
-    episodesURL
-      .then((response) => response.json())
-      .then((episode) => episode.characters)
+  )
+    .then((response) => response.json())
+    .then((episode) =>
+      episode.characters.map((charURL) => charURL.replace(characterURL, ""))
+    )
+    .then((id) => id.map((nr) => +nr));
+
+  const characterPromises = charactersIDs.map((id) =>
+    fetch(`${characterURL}${id}`).then((res) => res.json())
   );
 
-  return console.log(somearray);
+  return Promise.all(characterPromises);
 }
 
-getCharactersFromEpisode(7);
-// .then((characters) =>
-//   characters.forEach((character) => console.log(character))
-// );
+getCharactersFromEpisode(7).then((characters) =>
+  characters.forEach((character) => console.log(character.name))
+);
+ 
