8ad84956a5a09327a2d00a3a6d49a2f083a0e426
djakimiuk <djakimiuk@outlook.com>
2022-11-24 23:03:18 +0100

    ex. 3 solved

--- Praca domowa nr 4/homework-3.js
+++ Praca domowa nr 4/homework-3.js
@@ -9,13 +9,39 @@
 // Value can be type string or number.
 // reverseValue(): If number do (*(-1)), if string reverse it.  Closure pattern.
 
-const myObject = (() => {})(); // YOUR CODE HERE
+const myObject = (() => {
+  return {
+    setValue: (input) => {
+      if (typeof input === "string" || typeof input === "number") {
+        return (myObject.output = input);
+      } else {
+        throw new Error("Invalid input type!");
+      }
+    },
+    showValue: () => {
+      if (myObject.output) {
+        console.log(myObject.output);
+      } else {
+        throw new Error("There is no value!");
+      }
+    },
+    reverseValue: () => {
+      if (typeof myObject.output === "string") {
+        myObject.output = myObject.output.split("").reverse().join("");
+        console.log(myObject.output);
+      } else {
+        myObject.output = myObject.output * -1;
+        console.log(myObject.output);
+      }
+    },
+  };
+})(); // YOUR CODE HERE
 
-// myObject.setValue(5);
-// myObject.showValue();
-// myObject.reverseValue();
-// myObject.showValue();
-// myObject.setValue('hello');
-// myObject.showValue();
-// myObject.reverseValue();
-// myObject.showValue();
+myObject.setValue(5);
+myObject.showValue();
+myObject.reverseValue();
+myObject.showValue();
+myObject.setValue("hello");
+myObject.showValue();
+myObject.reverseValue();
+myObject.showValue();
